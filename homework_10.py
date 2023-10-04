348642835f20bc5df9ffc68ca8bb49e252e7f040
centr.yarik <centr.yarik@gmail.com>
2023-10-04 23:01:45 +0300

    homework_10.py H/W 10 Part 1/3 done

--- homework_10.py
+++ homework_10.py
@@ -1,41 +1,30 @@
-# def func(item):
-#     return f"{item}\n"
-#
-#
-# with open("text.txt", "r", encoding="utf-8") as f:
-#
-#     "w"
-#     x = ['1', '2', '3', '4']
-#     x = [f'{item}\n' for item in x]
-#     x = map(func, x)
-#     f.write('Hello\n')
-#     f.write('Python\n')
-#     f.write('Python Pro\n')
-#     f.writelines(x)
-#
-#     "r"
-#     while data := f.read(1):
-#         print(data)
-#     data = f.readlines()
-#     print(*data, sep="")
-#     for line in f:
-#         print(line.strip())
-#     import timeit
-#
-#     stmp_1 = """
-#     with open('test.txt', 'r', encoding='utf-8') as f:
-#         data = f.readlines()
-#         s = 0
-#         for item in data:
-#             s += int(item.strip())
-#     """
-#
-#     stmp_2 = """
-#     with open('test.txt', 'r', encoding='utf-8') as f:
-#         s = 0
-#         for item in f:
-#             s += int(item.strip())
-#     """
-#
-#     print(timeit.timeit(stmp_1, number=5))
-#     print(timeit.timeit(stmp_2, number=5))
+def category_money(family_data: dict) -> dict:
+    """
+    Returns dict with key = category, value = money spent on category
+
+    :param family_data: data extracted from .txt
+    :return: dict{key = category : value = money spent on that category}
+    """
+    categories_money_dict = {}
+    # Find all the categories
+    for values in family_data.values():
+        if values[-1] not in categories_money_dict:
+            categories_money_dict[values[-1]] = 0
+    # Find sum of spent money for all the categories
+    for key in categories_money_dict:
+        for values in family_data.values():
+            if values[-1] == key:
+                minus_dollar = values[-2]
+                minus_dollar = minus_dollar.strip('$')
+                categories_money_dict[key] += float(minus_dollar)
+        categories_money_dict[key] = f"{categories_money_dict[key]:.2f}$"
+    return categories_money_dict
+
+
+with open("hw_10_test.txt", "r", encoding="utf-8") as file:
+    data = {}
+    for item in file:
+        line = item.split()
+        transaction_id, *name, money, category = line
+        data[transaction_id] = (' '.join(name)), money, category
+    print(category_money(data))
