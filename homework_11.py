1727f85146408c10396c53cc558925d1085ebc20
centr.yarik <centr.yarik@gmail.com>
2023-10-10 23:57:45 +0300

    homework_11.py H/W 11 Next item for ANY SEQUENCE

--- homework_11.py
+++ homework_11.py
@@ -3,40 +3,50 @@ def parser(numbers: str) -> list:
 
 
 def is_arithmetic_sequence(numbers: list) -> bool:
-    # d2 = (((sum(numbers) * 2) / len(numbers)) - 2 * numbers[0]) / (len(numbers) - 1)
-    d = numbers[2] - numbers[1]
-    if numbers[0] + d == numbers[1] and numbers[0] + d*2 == numbers[2]:
-        return True
-    return False
+    d = numbers[1] - numbers[0]
+    for i in range(len(numbers) - 1):
+        if numbers[i + 1] - numbers[i] != d:
+            return False
+    return True
 
 
 def is_geometric_sequence(numbers: list) -> bool:
-    q = numbers[2] / numbers[1]
-    if numbers[0] * q == numbers[1] and numbers[0] * q**2 == numbers[2]:
-        return True
-    return False
+    if 0 in numbers:
+        return False
+    q = numbers[1] / numbers[0]
+    for i in range(len(numbers) - 1):
+        if numbers[i + 1] / numbers[i] != q:
+            return False
+    return True
 
 
 def next_arithmetic_item(numbers: list) -> int:
     # d2 = (((sum(numbers) * 2) / len(numbers)) - 2 * numbers[0]) / (len(numbers) - 1)
-    d = numbers[2] - numbers[1]
+    d = numbers[1] - numbers[0]
     return numbers[-1] + d
 
 
 def next_geometric_item(numbers: list) -> int:
-    q = (numbers[2] / numbers[1])
+    q = numbers[1] / numbers[0]
     return numbers[-1] * q
 
 
 def get_next_item(numbers: list):
-    if len(numbers) > 2:
+    if len(numbers) >= 2:
         if is_arithmetic_sequence(numbers):
-            print("Arithmetic")
             return next_arithmetic_item(numbers)
 
         if is_geometric_sequence(numbers):
-            print("Geometric")
             return next_geometric_item(numbers)
+
+        next_diff = get_next_item([numbers[i + 1] - numbers[i] for i in range(len(numbers) - 1)])
+        if next_diff is not None:
+            return numbers[-1] + next_diff
+        if 0 in numbers:
+            return None
+        next_div = get_next_item([numbers[i + 1] / numbers[i] for i in range(len(numbers) - 1)])
+        if next_div is not None:
+            return numbers[-1] * next_div
 
     return None
 
